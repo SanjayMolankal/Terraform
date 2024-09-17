d0cdc4505c77c0367bbfd8f8121ddf911cd331e4
Sanjay <139351494+SanjayMolankal@users.noreply.github.com>
2024-09-17 16:40:53 +0530

    Update terrascan.ts

--- terrascan.ts
+++ terrascan.ts
@@ -1,7 +1,7 @@
 import { exec } from 'child_process';
 
 // Function to install Terrascan
-const installTerrascan = () => {
+export const installTerrascan = () => {
   exec('curl -L https://github.com/tenable/terrascan/releases/latest/download/terrascan_linux_amd64.tar.gz | tar -xvz && sudo mv terrascan /usr/local/bin', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error installing Terrascan: ${error.message}`);
