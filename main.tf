ee7159aa47dae26254811278f5385db11bd99c3c
Sanjay <139351494+SanjayMolankal@users.noreply.github.com>
2024-09-21 22:00:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-terraform {
-  backend "s3" {
-    region  = "us-west-2"
-    bucket  = "San-S3-Bucket"
-    key     = "terraform.tfstate"
-    dynamodb_table = "terraform-lock-table"  # optional for state locking
-  }
-}
+#terraform {
+ # backend "s3" {
+  #  region  = "us-west-2"
+   # bucket  = "San-S3-Bucket"
+   # key     = "terraform.tfstate"
+    #dynamodb_table = "terraform-lock-table"  # optional for state locking
+  #}
+#}
 
- metadata_options {
-    http_tokens   = "required"  # Forces IMDSv2
-    http_endpoint = "enabled"   # Keep metadata endpoint enabled
-  }
+ #metadata_options {
+  #  http_tokens   = "required"  # Forces IMDSv2
+   # http_endpoint = "enabled"   # Keep metadata endpoint enabled
+  #}
 
-resource "aws_instance" "my_server" {
-  ami           = "ami-0a55ba1c20b74fc30"
-  instance_type = "t2.micro"
+#resource "aws_instance" "my_server" {
+ # ami           = "ami-0a55ba1c20b74fc30"
+  #instance_type = "t2.micro"
   
-  tags = {
-    Name = "My_New_Server"
-  }
-}
+  #tags = {
+   # Name = "My_New_Server"
+  #}
+#}
