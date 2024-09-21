64bdd9f7c0eb22c53770d185296591a7bdac6d34
Sanjay <139351494+SanjayMolankal@users.noreply.github.com>
2024-09-21 21:44:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,17 @@
-backend "s3" {
-   region = "us-west-2"
-   key    = "terraform.tfstate"
- }
-resource "aws_instance" "example_server" {
- ami = "ami-0a55ba1c20b74fc30"
- instance_type= "t2.micro"
- 
- tags = {
-  Name = "My_Ney_Server"
+terraform {
+  backend "s3" {
+    region  = "us-west-2"
+    bucket  = "your-s3-bucket-name"
+    key     = "terraform.tfstate"
+    dynamodb_table = "terraform-lock-table"  # optional for state locking
+  }
 }
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-0a55ba1c20b74fc30"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "My_New_Server"
+  }
 }
