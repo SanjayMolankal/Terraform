d462eefd54fc01c80ff2b66e65daf29688c852a0
Sanjay <139351494+SanjayMolankal@users.noreply.github.com>
2024-09-21 21:51:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,12 @@ terraform {
     dynamodb_table = "terraform-lock-table"  # optional for state locking
   }
 }
+
+ metadata_options {
+    http_tokens   = "required"  # Forces IMDSv2
+    http_endpoint = "enabled"   # Keep metadata endpoint enabled
+  }
+
 resource "aws_instance" "my_server" {
   ami           = "ami-0a55ba1c20b74fc30"
   instance_type = "t2.micro"
